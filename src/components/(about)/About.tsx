041e96e9ea6a96351a24e295b4fdc68ce311a61c
Abuhamida <mr6303302@gmail.com>
2026-03-31 19:20:37 +0200

    Updates: 2026-03-31T17:20:35.442Z

--- src/components/(about)/About.tsx
+++ src/components/(about)/About.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import Reveal from "@/components/animation/Reveal";
 import { motion, AnimatePresence, useScroll, useTransform } from "framer-motion";
 
